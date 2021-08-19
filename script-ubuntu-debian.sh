a09fe98fc35360e9d3ff9ba12f90d258bdb9451c
Kasper Ireland <54741819+kasperireland@users.noreply.github.com>
2021-08-19 15:29:43 -0400

    Update script-ubuntu-debian.sh

--- script-ubuntu-debian.sh
+++ script-ubuntu-debian.sh
@@ -1,8 +1,6 @@
 apt update
 apt install iptables-persistent
 /sbin/iptables -t mangle -A PREROUTING -m conntrack --ctstate INVALID -j DROP
-/sbin/iptables -t mangle -A PREROUTING -p tcp ! --syn -m conntrack --ctstate NEW -j DROP
-/sbin/iptables -t mangle -A PREROUTING -p tcp -m conntrack --ctstate NEW -m tcpmss ! --mss 536:65535 -j DROP
 /sbin/iptables -t mangle -A PREROUTING -p tcp --tcp-flags FIN,SYN,RST,PSH,ACK,URG NONE -j DROP
 /sbin/iptables -t mangle -A PREROUTING -p tcp --tcp-flags FIN,SYN FIN,SYN -j DROP
 /sbin/iptables -t mangle -A PREROUTING -p tcp --tcp-flags SYN,RST SYN,RST -j DROP
@@ -17,6 +15,7 @@ apt install iptables-persistent
 /sbin/iptables -t mangle -A PREROUTING -p tcp --tcp-flags ALL FIN,PSH,URG -j DROP
 /sbin/iptables -t mangle -A PREROUTING -p tcp --tcp-flags ALL SYN,FIN,PSH,URG -j DROP
 /sbin/iptables -t mangle -A PREROUTING -p tcp --tcp-flags ALL SYN,RST,ACK,FIN,URG -j DROP
+/sbin/iptables -t mangle -A PREROUTING -p icmp -j DROP
 /sbin/iptables -t mangle -A PREROUTING -s 224.0.0.0/3 -j DROP
 /sbin/iptables -t mangle -A PREROUTING -s 169.254.0.0/16 -j DROP
 /sbin/iptables -t mangle -A PREROUTING -s 172.16.0.0/12 -j DROP
@@ -26,15 +25,16 @@ apt install iptables-persistent
 /sbin/iptables -t mangle -A PREROUTING -s 0.0.0.0/8 -j DROP
 /sbin/iptables -t mangle -A PREROUTING -s 240.0.0.0/5 -j DROP
 /sbin/iptables -t mangle -A PREROUTING -s 127.0.0.0/8 ! -i lo -j DROP
-/sbin/iptables -t mangle -A PREROUTING -p icmp -j DROP
-/sbin/iptables -t mangle -A PREROUTING -f -j DROP
+/sbin/iptables -t mangle -A PREROUTING -p tcp -m conntrack --ctstate NEW -m tcpmss ! --mss 536:65535 -j DROP
+/sbin/iptables -t mangle -A PREROUTING -p tcp ! --syn -m conntrack --ctstate NEW -j DROP
 /sbin/iptables -A INPUT -p tcp -m connlimit --connlimit-above 111 -j REJECT --reject-with tcp-reset
 /sbin/iptables -A INPUT -p tcp --tcp-flags RST RST -m limit --limit 2/s --limit-burst 2 -j ACCEPT
 /sbin/iptables -A INPUT -p tcp --tcp-flags RST RST -j DROP
-/sbin/iptables -A INPUT -p tcp -m conntrack --ctstate NEW -m limit --limit 60/s --limit-burst 20 -j ACCEPT
-/sbin/iptables -A INPUT -p tcp -m conntrack --ctstate NEW -j DROP
-/sbin/iptables -A INPUT -p tcp --dport ssh -m conntrack --ctstate NEW -m recent --set
-/sbin/iptables -A INPUT -p tcp --dport ssh -m conntrack --ctstate NEW -m recent --update --seconds 60 --hitcount 10 -j DROP
+/sbin/iptables -t mangle -A PREROUTING -f -j DROP
 /sbin/iptables -N port-scanning
 /sbin/iptables -A port-scanning -p tcp --tcp-flags SYN,ACK,FIN,RST RST -m limit --limit 1/s --limit-burst 2 -j RETURN
 /sbin/iptables -A port-scanning -j DROP
+/sbin/iptables -A INPUT -p tcp --dport ssh -m conntrack --ctstate NEW -m recent --set
+/sbin/iptables -A INPUT -p tcp --dport ssh -m conntrack --ctstate NEW -m recent --update --seconds 60 --hitcount 10 -j DROP
+
+echo "Firewall configuration successfully applied."
